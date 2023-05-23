5ba2ab3db0c513266ccbf748f942ba6e66dec591
daiego <diedelcha@gmail.com>
2023-05-23 10:57:17 +0200

    Changes made to configuration steps

--- scripts/drum_setup.py
+++ scripts/drum_setup.py
@@ -6,11 +6,10 @@ import os
 import rospkg
 
 package_path = rospkg.RosPack().get_path("haru_drums_ros_driver")
-
 config_path = os.path.join(package_path, "config")
+sounds_path = os.path.join(package_path, "src", "sounds")
 
-colors = ["red", "yellow", "blue",
-          "green", "orange", "light-blue", "purple"]
+colors = ["red", "yellow", "blue", "green", "orange", "light-blue", "purple"]
 
 
 def read_drum_hit(port):
@@ -35,42 +34,85 @@ def read_drum_hit(port):
     return data["number"]
 
 
-print('Available ports')
-available_ports = []
-for i, port in enumerate(rtmidi.MidiIn().get_ports()):
-    print(i, "-", port)
-    available_ports.append(i)
+def setup_midi_port():
+    print("################################")
+    print('#       PORT SELECTION         #')
+    print('################################\n')
+    print("Available midi ports:")
 
-okay = False
-while not okay:
-    port = int(input("Select midi port:"))
-    if port in available_ports:
-        okay = True
+    available_ports = []
+    port = 0
+    for i, port in enumerate(rtmidi.MidiIn().get_ports()):
+        print(i, "-", port)
+        available_ports.append(i)
 
-print("For each color press the corresponding pad three times\n")
-drum_note_dict = {}
-drum_color_dict = {}
+    try:
+        port = int(input("Select midi port:"))
+    except IndexError:
+        setup_midi_port()
 
-for color in colors:
-    repeat = True
-    print(f"Hit {color}")
-    while repeat:
-        ans = read_drum_hit(port)
-        if len(set(ans)) == 1 and ans[0] not in drum_note_dict.keys():
-            repeat = False
-            drum_note_dict[ans[0]] = color
-            drum_color_dict[color] = ans[0]
-        else:
-            print(f"Try again with:{color}")
+    return port
 
-drum_data = {
-    "default_port": port,
-    "color_number": drum_color_dict,
-    "number_color": drum_note_dict
-}
 
-with open(os.path.join(config_path, "drum_settings.yaml"), "w") as file:
-    yaml.dump(drum_data, file)
+def setup_colors(port):
+    print("################################")
+    print('#    COLOR CONFIGURATION       #')
+    print('################################\n')
+    print("For each color press the corresponding pad three times\n")
+    drum_note_dict = {}
+    drum_color_dict = {}
 
-#################################
-print("Drum config file changed!")
+    for color in colors:
+        repeat = True
+        print(f"Hit {color}")
+        while repeat:
+            ans = read_drum_hit(port)
+            if len(set(ans)) == 1 and ans[0] not in drum_note_dict.keys():
+                repeat = False
+                drum_note_dict[ans[0]] = color
+                drum_color_dict[color] = ans[0]
+            else:
+                print(f"Try again with:{color}")
+    return drum_note_dict, drum_color_dict
+
+
+def setup_sound_set():
+    print("################################")
+    print('#   SOUND EFFECT SELECTION     #')
+    print('################################\n')
+
+    print("These are the detected sound effects")
+
+    for i, sound_set in enumerate(os.listdir(sounds_path)):
+        print(i, "-", sound_set)
+
+    try:
+        sound_set = os.path.join(sounds_path, os.listdir(sounds_path)[int(input("Select a sound set:"))])
+    except IndexError:
+        setup_sound_set()
+
+    sound_tracks = [os.path.join(sound_set, sound) for sound in os.listdir(sound_set)]
+
+    return sound_tracks
+
+
+def full_setup():
+    port = setup_midi_port()
+    number_color, color_number = setup_colors(port)
+    sound_tracks = setup_sound_set()
+    number_sound = dict(zip(number_color.keys(), sound_tracks))
+
+    drum_data = {
+        "default_port": port,
+        "color_number": color_number,
+        "number_color": number_color,
+        "number_sound": number_sound
+    }
+
+    with open(os.path.join(config_path, "drum_settings.yaml"), "w") as file:
+        yaml.dump(drum_data, file)
+    print("Drum config file changed!")
+
+
+if __name__ == '__main__':
+    full_setup()
--- scripts/midi_drum_node.py
+++ scripts/midi_drum_node.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
 """
-This module implements the midi_drums node.
-This node will publish and execute all actions related
-to the interaction with
+This module implements the Midi Drum Node.
+
+This node conects to the default midi port defined in the previous configuration steps.
+
+It publishes DrumMidiSignal msgs in midi_signal/hit topic
+And subscribes to DrumMidiSignal msgs in midi_signal/record
+
+
 """
 import os
-import rtmidi
-import rospy
-import yaml
-import rospkg
 import threading
-from haru_drums_ros_driver.msg import DrumMidiSignal
-from custom_sounds_module import play_sound
 
+import rospkg
+import rospy
+import rtmidi
+import yaml
+from haru_drums_ros_driver.msg import DrumMidiSignal
+from pydub import AudioSegment
+from pydub.playback import play
 
 package_path = rospkg.RosPack().get_path("haru_drums_ros_driver")
-config_path = os.path.join(package_path, "config")
-settings_path = os.path.join(config_path, "drum_settings.yaml")
+settings_path = os.path.join(package_path, "config", "drum_settings.yaml")
+sound_folder = os.path.join(package_path, "src", "sounds", "sound_set_1")
+
+with open(settings_path, "r") as file:
+    settings = yaml.safe_load(file)
 
 
 class MidiDrum():
@@ -26,6 +35,7 @@ class MidiDrum():
         self.port = settings["default_port"]
         self.color_to_num_dict = settings["color_number"]
         self.num_to_color_dict = settings["number_color"]
+        self.num_to_sound_dict = settings["number_sound"]
         self.midi_signal_pub = rospy.Publisher('midi_signal/hit', DrumMidiSignal, queue_size=10)
         self.midi_record_sus = rospy.Subscriber('midi_signal/record', DrumMidiSignal, self.play_record)
         self.custom_sounds = custom_sounds
@@ -33,11 +43,16 @@ class MidiDrum():
         self.midi_in.open_port(self.port)
         self.midi_in.set_callback(self.handle)
 
+    def play_sound(self, number):
+        mp3_file = self.num_to_sound_dict[number]
+        sound = AudioSegment.from_mp3(mp3_file)
+        play(sound)
+
     def play_record(self, signal_msg: DrumMidiSignal):
         self.play_sound_async(signal_msg.midi_key)
 
     def play_sound_async(self, midi_code):
-        thread = threading.Thread(target=play_sound, args=(midi_code,), daemon=True)
+        thread = threading.Thread(target=self.play_sound, args=(midi_code,), daemon=True)
         thread.start()
 
     def handle(self, event, data):
@@ -53,11 +68,9 @@ class MidiDrum():
     def shutdown(self):
         self.midi_in.close_port()
 
-
-
     def __str__(self):
         s: str = f"Midi Drum\n-------------------------------------\nListening through port: {self.port}\n-------------------------------------\n" \
-            f"Color dictionary:\n\n"
+                 f"Color dictionary:\n\n"
         for key, value in self.num_to_color_dict.items():
             s += f"{key} <-----> {value}\n"
         return s
